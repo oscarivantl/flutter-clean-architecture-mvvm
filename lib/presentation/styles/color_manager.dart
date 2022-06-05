f91e777e39965d2588f7536daeefd356af110bf5
Oscar Ivan Torres Lopez <oscar.torres@unipamplona.edu.co>
2022-06-05 17:30:49 -0500

    added colors manager

---
+++ lib/presentation/styles/color_manager.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class ColorManager {
+  static Color primary = HexColor.fromHex("#035bf9");
+  static Color secundary = HexColor.fromHex("#4749aa");
+
+  static Color blueDark1 = HexColor.fromHex("#002d68");
+  static Color blueDark2 = HexColor.fromHex("#123850");
+}
+
+extension HexColor on Color {
+  static Color fromHex(String hexColorString){
+    hexColorString = hexColorString.replaceAll('#', '');
+    if(hexColorString.length == 6){
+      //hexColorString = "FF" + hexColorString; // 8 char with opacity 100%
+      hexColorString = "FF $hexColorString";
+    }
+    return Color(int.parse(hexColorString, radix: 16));
+  }
+}
